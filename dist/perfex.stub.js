4216d7c229ddf339a91926820b56be3e90fd89e0
BSick7 <brad.sickles@gmail.com>
2015-01-24 09:20:06 -0500

    Updated stub.

--- dist/perfex.stub.js
+++ dist/perfex.stub.js
@@ -1,19 +1,24 @@
-if (!perfex.timer) {
-    perfex.timer = {
-        all: [],
-        reset: function () {
-        },
-        start: function (tag) {
-        },
-        stop: function () {
-        }
-    };
-}
-if (!perfex.phases) {
-    perfex.phases = {
-        current: null,
-        all: [],
-        start: function (tag) {
-        }
-    };
-}
+(function (context) {
+    if (!context.perfex) {
+        context.perfex = {};
+    }
+    if (!context.perfex.timer) {
+        context.perfex.timer = {
+            all: [],
+            reset: function () {
+            },
+            start: function (tag) {
+            },
+            stop: function () {
+            }
+        };
+    }
+    if (!context.perfex.phases) {
+        context.perfex.phases = {
+            current: null,
+            all: [],
+            start: function (tag) {
+            }
+        };
+    }
+})(window);
